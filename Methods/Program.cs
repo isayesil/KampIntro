92833fdf71926cee269c668bf11e7460da2086c6
İSA YEŞİLBAŞ <isa_yesilbas08@hotmail.com>
2021-01-24 00:54:32 +0300

    24.01.2021

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            var result=Add2(20, 30);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+          static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+    }
+}
